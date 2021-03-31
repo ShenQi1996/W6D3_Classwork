76bd38627e9364a3ffca69307eafb39f29b4ac88
tahj <tahjharris00@gmail.com>
2021-03-31 14:45:22 -0500

    create routes and action methods as well as a table and model

--- First_Routes_Controllers/app/controllers/users_controller.rb
+++ First_Routes_Controllers/app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
     end
 
 
@@ -10,10 +11,40 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: params [:sam]
+        user = User.find(params[:id])
+        render json: user
 
     end
 
+    def create
+        user = User.new(user_params)
+        if user.save
+            # render json: user
+            redirect_to users_url
+        else
+            render json: user.errors.full_message, status: 422
+        end
+    end
 
+    def update
+        user = User.find(params[:id])
+       if user.update(user_params) 
+            # render json: user
+            redirect_to user_url(user.id)
+       else
+        render json: user.errors.full_message, status: 422
+       end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
     
 end
